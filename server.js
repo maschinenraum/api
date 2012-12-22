4b2a143909ce263f1270e52a04109390ef0ac7f8
Max F. Albrecht <mfa@Quince.178.is>
2012-12-22 15:08:28 +0100

    implement a simple router, output the route ("Hello World from /route")
    from http://www.nodebeginner.org/#whats-needed-to-route-requests

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-// use 'http' module (aka server)
+// require modules
+// - use 'http' module (aka server)
 var http = require("http");
+// user 'url' module (get info about the request)
+var url = require("url");
 
 // declare function to run the server
 function start() {
     // declare a function to answer requests to the server
     function onRequest (request, response) {
-        console.log("Request received.")
+        // declare var holding the pathname
+        var pathname = url.parse(request.url).pathname;
+
+        console.log("Request for " + pathname + " received.");
+        
+        // build the response
         response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World");
+        response.write("Hello World from " + pathname);
         response.end();
     }
     // start the server on port 8888, define 'onRequest' as the callback function
