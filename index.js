e05e0da4b565468cf6313530587f1240477f5435
Max F. Albrecht <mfa@Quince.178.is>
2012-12-22 14:46:52 +0100

    refactor server into module
    from http://www.nodebeginner.org/#finding-a-place-for-our-server-module

---
+++ index.js
@@ -0,0 +1,5 @@
+// import modules
+var server = require("./server");
+
+// start server
+server.start();
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 // use 'http' module (aka server)
 var http = require("http");
 
-// declare a function to answer requests to the server
-function onRequest (request, response) {
-    console.log("Request received.")
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-}
-// start the server on port 8888, define 'onRequest' as the callback function
-http.createServer(onRequest).listen(8888);
+// declare function to run the server
+function start() {
+    // declare a function to answer requests to the server
+    function onRequest (request, response) {
+        console.log("Request received.")
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+    // start the server on port 8888, define 'onRequest' as the callback function
+    http.createServer(onRequest).listen(8888);
 
-console.log("Server has started.");
+    console.log("Server has started.");    
+}
+
+// export our start function (map public method to internal implementation function)
+exports.start = start;
