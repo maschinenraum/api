a270fcb177a55bb108641055f6c93106fd9bb30d
Max F. Albrecht <mfa@Quince.178.is>
2012-12-23 01:13:58 +0100

    router: handle path /spaceapi.json

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
+// Router: setup
 // make object mapping paths to 'request.Handlers' functions
 var handle = {}
 handle["/"] = requestHandlers.start;
 handle["/info"] = requestHandlers.info;
+handle["/spaceapi.json"] = requestHandlers.info;
 
 // start server
 server.start(router.route, handle);
